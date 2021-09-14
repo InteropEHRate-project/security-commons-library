86272d4c6f8df1d015162a23f0044b45dee606e1
Menelaos Giannopoulos <mgiannopoulos@ubitech.eu>
2021-09-14 14:17:59 +0300

    Close httpClient in a more appropriate way

--- src/main/java/eu/interopehrate/security_commons/services/SecurityHttpClient.java
+++ src/main/java/eu/interopehrate/security_commons/services/SecurityHttpClient.java
@@ -18,24 +18,26 @@ public class SecurityHttpClient {
 
     public static String createPost(String url, String body) throws IOException, ExecutionException, InterruptedException {
         CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
-        client.start();
-        HttpPost request = new HttpPost(url);
-        request.setHeader("Accept", "application/json");
-        request.setHeader("Content-type", "application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        request.setEntity(new StringEntity(body, "UTF-8"));
-        Future<HttpResponse> future = client.execute(request, null);
-        HttpResponse response = future.get();
-        client.close();
-        int status = response.getStatusLine().getStatusCode();
-        HttpEntity entity = response.getEntity();
-        String result = entity != null ? EntityUtils.toString(entity) : null;
-        if (status >= 200 && status < 300) {
-            return result;
-        } else {
-            throw new ClientProtocolException("Unexpected response status: " + status + " " + result);
+
+        try {
+            client.start();
+            HttpPost request = new HttpPost(url);
+            request.setHeader("Accept", "application/json");
+            request.setHeader("Content-type", "application/json");
+            ObjectMapper mapper = new ObjectMapper();
+            request.setEntity(new StringEntity(body, "UTF-8"));
+            Future<HttpResponse> future = client.execute(request, null);
+            HttpResponse response = future.get();
+            int status = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+            String result = entity != null ? EntityUtils.toString(entity) : null;
+            if (status >= 200 && status < 300) {
+                return result;
+            } else {
+                throw new ClientProtocolException("Unexpected response status: " + status + " " + result);
+            }
+        } finally {
+            client.close();
         }
     }
-
-
 }
